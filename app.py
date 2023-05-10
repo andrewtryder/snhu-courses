7da63e9eb618ab2f3c4e9ed939253a86490b30b4
Andrew Ryder <1811535+andrewtryder@users.noreply.github.com>
2023-05-10 14:47:32 -0400

    Updates.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
-from course_tree_generator import create_course_tree
+from course_tree_generator import create_course_tree, create_courses_trees
 from database import get_course_info, get_courses_info
 from typing import Union, Tuple, Dict
 from flask_cors import CORS
@@ -9,46 +9,69 @@ import json
 app = Flask(__name__, template_folder='templates', static_folder='static')
 CORS(app)
 
-def format_data(data):
+def format_data(data, node_ids, edge_ids):
     nodes = []
     edges = []
 
     def traverse(node, parent=None):
-        nodes.append({
-            'id': node['course_id'],
-            'label': node['course_id'],
-            'title': node['name']
-        })
-        if parent:
-            edges.append({'from': parent, 'to': node['course_id']})
+        node_id = node['course_id']
+        if node_id not in node_ids:
+            # Add node to list of nodes if it hasn't been added already
+            nodes.append({
+                'id': node_id,
+                'label': node_id,
+                'title': node['name']
+            })
+            node_ids.add(node_id)
+
+        if parent and (parent, node_id) not in edge_ids:
+            # Add edge to list of edges if it hasn't been added already
+            edges.append({'from': parent, 'to': node_id})
+            edge_ids.add((parent, node_id))
+
         if 'prerequisites' in node:
             for child in node['prerequisites']:
-                traverse(child, node['course_id'])
+                traverse(child, node_id)
 
     traverse(data)
     return {'nodes': nodes, 'edges': edges}
 
 
 @app.route('/', methods=['GET', 'POST'])
-def course_graph():
+def course_graphs():
     if request.method == 'POST':
-        # Get course name from form data
-        course_name = request.form['course_name']
-        # Validate course name against database
-        course_info = get_course_info(course_name)
-        if course_info:
-            # Make API call to get latest data
-            data = create_course_tree(course_name)
-            # Format data for vis.js consumption
-            vis_data = format_data(data)
+        # Get course names from form data
+        course_input = request.form['course_name']
+        course_names = [course.strip().upper() for course in course_input.split(',')]
+        courses_data = []
+        node_ids = set()
+        edge_ids = set()
 
-            # Render graph onto webpage
-            return render_template('index.html', course_name=course_name, graph=render_template('course-graph.html', data=json.dumps(vis_data)))
-        else:
-            # Invalid course name, render error page
-            return render_template('index.html', error='Invalid course name.')
+        # Validate course names and get course data
+        for course_name in course_names:
+            course_info = get_course_info(course_name)
+            if course_info:
+                data = create_course_tree(course_name)
+                course_data = format_data(data, node_ids, edge_ids)
+                courses_data.append(course_data)
+            else:
+                # Invalid course name, render error page
+                return render_template('index.html', error='{0} Invalid course name.'.format(course_name))
+
+        # Combine nodes and edges from all courses
+        nodes = []
+        edges = []
+
+        for course_data in courses_data:
+            nodes += course_data['nodes']
+            edges += course_data['edges']
+
+        vis_data = {'nodes': nodes, 'edges': edges}
+        #print("VISDATA :: {0}".format(vis_data))
+        # Render graph onto webpage
+        return render_template('index.html', course_names=course_names, graph=render_template('course-graph.html', data=json.dumps(vis_data)))
     else:
-        # Show form to ask for course name
+        # Show form to ask for course names
         return render_template('index.html')
 
 @app.route('/course-trees/<course_ids>')
